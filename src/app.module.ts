2ece073a18920e8137bb8817d290c43445bb6796
rodzy <irod2899@gmail.com>
2020-12-08 00:47:23 +0000

    Added first module and schema first aproach

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
